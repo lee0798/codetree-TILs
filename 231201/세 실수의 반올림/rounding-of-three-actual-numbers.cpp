11b58a82d1b2f45be654b6bdecba87c54d1380b1
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-01 16:25:09 +0900

    Title: 세 실수의 반올림, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231201/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c;
+    cin >>a >>b >.c;
+    cout<<fixed;
+    cout.precision(3);
+    cout << a <<endl;
+    cout << b <<endl;
+    cout << c <<endl;
+    return 0;
+}
