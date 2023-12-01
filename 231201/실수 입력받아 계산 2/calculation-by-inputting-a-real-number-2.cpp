b2769e9b49a3c4f3c009141ad7ed3bffc8ef568e
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-01 16:13:44 +0900

    Title: 실수 입력받아 계산 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231201/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
+++ 231201/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-
+using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     double a;
     cout<< fixed;
     cout.precision(2);
     cout << a + 1.5;
-    
+
     return 0;
 }
