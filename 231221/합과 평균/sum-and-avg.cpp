5116d9a3e3d5fed22145ffc3cd8709ee4d2a35be
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-21 15:45:08 +0900

    Title: 합과 평균, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231221/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a , b;
+    cin >> a >> b;
+    cout << a + b <<" ";
+    cout << fixed
+    cout.precsion(1);
+    cout <<(double)(a +b)/2;
+
+    return 0;
+}
