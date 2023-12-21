df0bb24473e5132146697e6a2c5f24ca604c6413
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-21 16:52:05 +0900

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231221/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a;
+
+	cin >> a;
+
+	if (a >= 3000) {
+		cout << "book" << endl;
+	}
+	else if (a >= 1000) {
+		cout << "mask" << endl;
+	}
+	else if(a >= 500){
+		cout << "pen" << endl;
+	}else{
+        cout << "no" <<endl;
+    }
+
+
+	return 0;
+
+}
