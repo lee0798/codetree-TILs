1df44f764e4ad6c9ec5e455d092e62649d270c10
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-21 15:50:28 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231221/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a ,b ,c ,d ;
+    cin >> a >> b >> c;
+    cout << a + b + c << endl;
+    d = (a +b + c)/3;
+    cout << d << endl;
+    cout << d *2;
+
+    return 0;
+}
