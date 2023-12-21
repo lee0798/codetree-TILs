1a48f0e9157c03cc0fc05249e295b01cffa732a9
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-21 15:06:06 +0900

    Title: 간단한 사칙연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231221/간단한 사칙연산/simple-arithmetic-operation.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cin >> a ;
+    cin >> b ;
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a / b << endl;
+    cout << a % b << endl;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
