5741db1e59b1ce39d0ff9f60b35aa6b6752e87eb
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-21 16:37:24 +0900

    Title: 등급 매기기, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231221/등급 매기기/ranking.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a;
+	scanf("%d", &a);
+
+	if (a >= 90) {
+		cout << "A" << endl;
+	}
+	else if (a >= 80) {
+		cout << "B" << endl;
+	}
+	else if (a >= 70) {
+		cout << "C" << endl;
+	}
+	else if(a >= 60){
+		cout << "D" << endl;
+	}else{
+        cout << "F";
+    }
+
+
+	return 0;
+
+}
