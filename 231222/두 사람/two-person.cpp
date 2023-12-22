2068d16800dab9db4f8472be85171a9406c76d1a
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-22 16:30:50 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231222/두 사람/two-person.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a ,b;
+    char c,d;
+    cin >> a >>c >>b >>d;
+    cout << ((a >=20 && c =='M')||(a >=20 && c =='M'))<<endl;
+    
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
