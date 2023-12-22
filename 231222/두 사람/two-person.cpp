88c524cc02ac4a9ddbd7b9b1008f27497742e8fc
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-22 16:31:06 +0900

    Title: 두 사람, Time: 14ms, Memory: 0MB, Status: Passed - CodeTree

--- 231222/두 사람/two-person.cpp
+++ 231222/두 사람/two-person.cpp
@@ -4,7 +4,7 @@ int main() {
     int a ,b;
     char c,d;
     cin >> a >>c >>b >>d;
-    cout << ((a >=20 && c =='M')||(a >=20 && c =='M'))<<endl;
+    cout << ((a >=20 && c =='M')||(b >=20 && d =='M'))<<endl;
     
     // 여기에 코드를 작성해주세요.
     return 0;
