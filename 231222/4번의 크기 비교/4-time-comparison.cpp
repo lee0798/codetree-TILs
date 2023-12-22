036a10763774fc568bfca6d71037de5f5212af3e
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-22 15:45:52 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231222/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 변수 선언
+    int a, b, c ,d ,e;
+
+	// 입력
+	cin >> a >> b >> c >> d >> e;
+    
+    // 출력
+    cout << (a >b) << endl;
+	cout << (a > c) << endl;
+	cout << (a >d) << endl;
+	cout << (a > e) << endl;
+
+	
+    return 0;
+}
