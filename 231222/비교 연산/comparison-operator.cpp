a65dc961d6fb44c0ac52a1cac16b4f81e4b014a8
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-22 15:40:48 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231222/비교 연산/comparison-operator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 변수 선언
+    int a, b;
+
+	// 입력
+	cin >> a >> b;
+    
+    // 출력
+    cout << (a >= b) << endl;
+	cout << (a > b) << endl;
+	cout << (a <= b) << endl;
+	cout << (a < b) << endl;
+	cout << (a == b) << endl;
+	cout << (a != b) << endl;
+	
+    return 0;
+}
