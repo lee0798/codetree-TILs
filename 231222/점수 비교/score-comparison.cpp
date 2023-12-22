6aff42358827438fd51f929faee2f2d4dfe27451
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-22 15:47:27 +0900

    Title: 점수 비교, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231222/점수 비교/score-comparison.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a,b;
+    int a1,b1;
+	cin >> a >> b;
+    cin >>a1 >> b1;
+	if (a > a1 && b > b1) {
+		cout << "1";
+	}
+	else {
+		cout << "0";
+	}
+
+	return 0;
+
+}
