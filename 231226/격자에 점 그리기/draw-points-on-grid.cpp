7fa5da15827c73caecc5fdfb16a5e4e9cde69548
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-26 15:07:13 +0900

    Title: 격자에 점 그리기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231226/격자에 점 그리기/draw-points-on-grid.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,m;
+    cin >> n >> m;
+    int arr[n][n] ={};
+    int num =1;
+    for(int i = 0; i < m; i ++){
+        int a,b ;
+        cin >> a >> b;
+        arr[a-1][b-1] = num;
+        num ++;
+    }
+    for(int i = 0; i < n ; i ++){
+        for(int j =0 ; j < n ; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
