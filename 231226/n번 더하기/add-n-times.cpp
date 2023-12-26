8e0c3362fdb96764061a3bc01702bef4442a3964
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-26 10:31:33 +0900

    Title: n번 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231226/n번 더하기/add-n-times.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a,b;
+    cin >> a >> b;
+
+	for (int i = 1; i <= b; i++) {
+		a += b;
+        cout << a << endl;;
+
+	}
+
+	return 0;
+
+}
