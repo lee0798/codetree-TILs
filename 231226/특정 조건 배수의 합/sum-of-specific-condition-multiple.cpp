5ce4902055917732840d219bc310c8fa77ffe60b
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-26 16:46:53 +0900

    Title: 특정 조건 배수의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231226/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
+++ 231226/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
@@ -5,6 +5,11 @@ int main() {
 
 	int a,b;
 	cin >> a >> b;
+    if (a > b){
+        int temp =b;
+        b =a;
+        a =temp;
+    }
     int sum = 0;
 	for (int i = a; i <= b; i++) {
 		if(i % 5 == 0){
