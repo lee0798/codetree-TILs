ea6c3cd29110dc875183512e2738176c69298ab3
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-27 10:45:37 +0900

    Title: 단어로 구분하기 3, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231227/단어로 구분하기 3/separate-words-with-words-3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    n =10;
+
+	string *arr = new string[n];
+
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	for (int i = n-1; i >= 0 ; i--) {
+        cout << arr[i] << endl;
+	}
+
+
+	return 0;
+
+}
