f05be4aa86d732b6aa897b2fc288fc24d7aa8c3c
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-27 13:09:21 +0900

    Title: 문자열에서 문자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231227/문자열에서 문자 출력/print-chars-from-word.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	string arr;
+	cin >> arr;
+	int len = arr.length();
+
+	for (int i = 0; i < len; i++) {
+		cout << arr[i] << endl;
+	}
+
+	return 0;
+
+}
