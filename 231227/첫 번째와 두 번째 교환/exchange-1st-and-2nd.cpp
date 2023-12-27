a45ab6ef826c0ad9212760803c66159ae4d1eb6b
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-27 14:45:58 +0900

    Title: 첫 번째와 두 번째 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231227/첫 번째와 두 번째 교환/exchange-1st-and-2nd.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	string s = "";
+    cin >> s;
+    char fir;
+    char sec;
+    fir = s[0];
+    sec = s[1];
+    for(int i = 0; i < s.length(); i ++){
+        if(s[i] == fir){
+            s[i] = sec;
+        }else if(s[i] == sec){
+            s[i] = fir;
+         }
+    }
+	cout << s;
+
+	return 0;
+
+}
