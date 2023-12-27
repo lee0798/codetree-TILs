8f274c7cd24c6591ba96d9f78d663777cdcc5f62
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-27 09:27:29 +0900

    Title: 문자열의 길이 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231227/문자열의 길이 출력하기/print-string's-range.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str, str1;
+    cin >> str >> str1;
+    cout << str.length() + str1.length();
+
+    return 0;
+
+}
