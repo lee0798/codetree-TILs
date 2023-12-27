a60fde1e698e8ef5d2800f02674baa0c04de025d
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-27 09:29:36 +0900

    Title: 더 긴 문자열, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231227/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str, str1;
+    cin >> str >> str1;
+    if(str.length() == str1.length()){
+        cout << "same" ;
+    }else{
+        cout << "Coding 6";
+    }
+
+    return 0;
+
+}
