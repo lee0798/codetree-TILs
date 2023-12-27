f25c85f44a47e854f266eb33088e7eda58db6b3e
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-27 14:03:57 +0900

    Title: 문자열의 특정 위치 찾기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231227/문자열의 특정 위치 찾기/find-specific-location-in-spring.cpp
+++ 231227/문자열의 특정 위치 찾기/find-specific-location-in-spring.cpp
@@ -11,7 +11,7 @@ int main() {
 	int length = s.length();
 	int start_idx = -1;
 
-	for (int i = 0; i < length - 1; i++) {
+	for (int i = 0; i <= length - 1; i++) {
 		if (s[i] == c[0] ) {
 			start_idx = i;
 			break;
