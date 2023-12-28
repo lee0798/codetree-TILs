5599610268d506bee778b92669344e57811da0b8
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-28 14:36:35 +0900

    Title: 아스키코드 변환, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231228/아스키코드 변환/convert-to-ascii.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	char s ;
+    int a ;
+    cin >> s >> a; 
+    
+    cout << (int)s <<" " << (char)a;
+	
+	
+	return 0;
+
+}
