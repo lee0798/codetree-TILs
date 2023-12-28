fed5b39abf0e01a8e6115d24411be61a9de1d036
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-28 15:22:34 +0900

    Title: 정수만 추출하기, Time: 8ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 231228/정수만 추출하기/extract-only-integers.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	string a ="";
+    string b = "";
+    string temp = "";
+    int c = 0,d = 0;
+	cin >> a >> b;
+    for(int i = 0 ; i < a.length(); i ++){
+        if(isdigit(a[i])){
+            temp +=a[i];
+        }else{
+            c = stoi(temp);
+            temp = "";
+            break;
+        }
+    }
+    for(int i = 0 ; i < b.length(); i ++){
+        if(isdigit(b[i])){
+            temp +=b[i];
+        }else{
+            d = stoi(temp);
+            break;
+        }
+    }
+    
+
+	cout << c + d;
+
+	return 0;
+
+}
