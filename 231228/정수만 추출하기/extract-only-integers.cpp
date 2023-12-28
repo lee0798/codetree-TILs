4151f95f7ab94a8d12f3335474beb543b9efc760
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-28 15:50:07 +0900

    Title: 정수만 추출하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231228/정수만 추출하기/extract-only-integers.cpp
+++ 231228/정수만 추출하기/extract-only-integers.cpp
@@ -7,27 +7,40 @@ int main() {
     string b = "";
     string temp = "";
     string temp2 = "";
-    int c = 0, d = 0;
+    int c = 0;
+    int d = 0;
 
     cin >> a >> b;
 
-    for (int i = 0; i < a.length(); i++) {
-        if (isdigit(a[i])) {
-            temp += a[i];
-        } else if (!temp.empty()) {
-            c = stoi(temp);
-            temp = "";
+    // Handle non-digit characters at the beginning of 'a'
+    int i = 0;
+    while (i < a.length() && !isdigit(a[i])) {
+        temp += a[i];
+        i++;
+    }
+
+    // Reset 'temp' before accumulating digits from 'a'
+    temp = "";
+    while (i < a.length() && isdigit(a[i])) {
+        temp += a[i];
+        i++;
+    }
+
+    c = stoi(temp);
+
+    // Accumulate digits from 'b'
+    temp2 = "";
+    for (int i = 0; i < b.length(); i++) {
+        if (isdigit(b[i])) {
+            temp2 += b[i];
+        } else {
             break;
         }
     }
 
-    for (int i = 0; i < b.length(); i++) {
-        if (isdigit(b[i])) {
-            temp2 += b[i];
-        } else{
-            d = stoi(temp2);
-            break;  
-        }
+    // Convert 'temp2' to integer only if it's not empty
+    if (!temp2.empty()) {
+        d = stoi(temp2);
     }
 
     cout << c + d;
