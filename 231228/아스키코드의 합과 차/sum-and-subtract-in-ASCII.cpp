5261c0b24afcffc57479d2a1fd2f26d215ad87fa
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-28 14:32:20 +0900

    Title: 아스키코드의 합과 차, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231228/아스키코드의 합과 차/sum-and-subtract-in-ASCII.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	char s ;
+    char n ;
+    cin >> s >> n; 
+    int a = (int)s;
+    int b = (int)n;
+    cout << a + b <<" " << b-a;
+	
+	
+	return 0;
+
+}
