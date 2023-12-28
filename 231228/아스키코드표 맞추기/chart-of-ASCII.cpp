8aa41b357c2ebb118624a729a0d6efc82a41c5dc
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-28 14:35:10 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231228/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	int arr[5] ;
+    for(int i = 0; i < 5; i ++){
+        cin >> arr[i];
+        cout << (char)arr[i] << " ";
+    }
+    
+	
+	
+	return 0;
+
+}
