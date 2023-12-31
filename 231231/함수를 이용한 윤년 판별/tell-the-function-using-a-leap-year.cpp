2f71b6751dd392230a20d02b6445929b78455810
이도환 <71830104+lee0798@users.noreply.github.com>
2023-12-31 19:14:29 +0900

    Title: 함수를 이용한 윤년 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231231/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
+++ 231231/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
@@ -3,8 +3,8 @@ using namespace std;
 bool fun(int n){
     if(n % 4 != 0)
     return false;
-    if(n % 100 == 0)
-    return false;
+    if(n % 100 != 0)
+    return true;
     if(n % 400 == 0)
     return true;
     return false;
