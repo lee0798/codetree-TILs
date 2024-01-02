6386ef38e3f8f7bf0a70f3ef3b6e8ec935706b0b
이도환 <71830104+lee0798@users.noreply.github.com>
2024-01-02 14:00:26 +0900

    Title: 재귀함수의 꽃, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240102/재귀함수의 꽃/the-flower-of-the-recursive-function.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+void PrintStar(int n) {  // 1부터 n번째 줄까지 별을 출력하는 함수
+    if(n == 0)           // n이 0이라면, 더 이상 진행하지 않고
+        return;          // 퇴각합니다.
+    cout << n << " ";
+    PrintStar(n - 1);   
+    cout << n << " ";
+}
+
+
+
+int main() {
+    int n;
+    cin >> n;
+    PrintStar(n);
+    cout << endl;
+
+    return 0;
+}
