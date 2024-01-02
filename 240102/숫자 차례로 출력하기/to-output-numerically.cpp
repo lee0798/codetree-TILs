bc93523a64a75f997580f52482e19d499ec92fdf
이도환 <71830104+lee0798@users.noreply.github.com>
2024-01-02 13:55:24 +0900

    Title: 숫자 차례로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240102/숫자 차례로 출력하기/to-output-numerically.cpp
+++ 240102/숫자 차례로 출력하기/to-output-numerically.cpp
@@ -4,14 +4,14 @@ void PrintStar(int n) {  // 1부터 n번째 줄까지 별을 출력하는 함수
     if(n == 0)           // n이 0이라면, 더 이상 진행하지 않고
         return;          // 퇴각합니다.
     
-    PrintStar(n - 1);    // 1부터 n - 1번째 줄까지 출력하는 함수
+    PrintStar(n - 1);   
     cout << n << " ";
 }
 void PrintStar2(int n) {  // 1부터 n번째 줄까지 별을 출력하는 함수
     if(n == 0)           // n이 0이라면, 더 이상 진행하지 않고
         return;          // 퇴각합니다.
     cout << n << " ";
-    PrintStar(n - 1);    // 1부터 n - 1번째 줄까지 출력하는 함수
+    PrintStar2(n - 1);    
 }
 
 
