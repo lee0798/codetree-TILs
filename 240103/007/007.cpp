0ed1c8c5534df428eadd447bf0b498d485f6703d
이도환 <71830104+lee0798@users.noreply.github.com>
2024-01-03 10:35:29 +0900

    Title: 007, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240103/007/007.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Spy {
+    public:
+        string code;
+        char point;
+        int time;
+        Spy(string code, char point, int time) {
+            this->code = code;
+            this->point = point;
+            this->time = time;
+        }
+};
+
+int main(){
+    // 변수 선언 및 입력
+    string scode;
+    char m;
+    int time;
+    cin >> scode >> m >> time;
+    
+    // Spy 객체를 만들어 줍니다.
+    Spy s = Spy(scode, m, time);
+
+    // 결과를 출력합니다.
+    cout << "secret code : " << s.code << endl;
+    cout << "meeting point : " << s.point << endl;
+    cout << "time : " << s.time << endl;
+    return 0;
+}
