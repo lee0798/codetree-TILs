caccafab38b00432d1369a69a1e930bbdaa7771c
이도환 <71830104+lee0798@users.noreply.github.com>
2024-01-04 11:25:07 +0900

    Title: 줄 세우기 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240104/줄 세우기 2/line-up-students-2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+class Student {
+    public:
+        int height, weight, num;
+
+        Student(int height, int weight , int num) {
+            this->height = height;
+            this->weight = weight;
+            this->num = num;
+        }
+        Student(){}
+};
+
+bool cmp(Student a, Student b) { 
+    if(a.height == b.height){
+        return a.weight > b.weight;
+    }
+    return a.height < b.height;
+         
+}
+
+int main() {
+    int n;
+    cin >> n;
+    Student students[1000] = {};
+    for(int i = 0 ; i < n ; i ++){
+        students[i].num = i+1;
+        cin >> students[i].height >>  students[i].weight;
+    }
+
+    sort(students, students + n, cmp); 
+
+    for(int i = 0; i < n; i++)
+        cout << students[i].height << " " << students[i].weight<< " " << students[i].num<< endl;
+
+    return 0;
+}
