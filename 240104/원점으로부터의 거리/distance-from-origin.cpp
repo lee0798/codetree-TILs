86d898915c2f26c4e78eef6815afb8010557f859
이도환 <71830104+lee0798@users.noreply.github.com>
2024-01-04 10:43:09 +0900

    Title: 원점으로부터의 거리, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240104/원점으로부터의 거리/distance-from-origin.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+class Student {
+    public:
+        int height, weight, num;
+
+        Student(int height, int weight , int num) {
+            this->height = height;
+            this->weight = weight;
+            this->num = num;
+        }
+        Student(){}
+};
+
+bool cmp(Student a, Student b) { 
+    return abs(a.height) + abs(a.weight) < abs(b.height) + abs(b.weight);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    Student students[1000] = {};
+    for(int i = 0 ; i < n ; i ++){
+        students[i].num = i+1;
+        cin >> students[i].height >>  students[i].weight;
+    }
+
+    sort(students, students + n, cmp); 
+
+    for(int i = 0; i < n; i++)
+        cout << students[i].num<< endl;
+
+    return 0;
+}
