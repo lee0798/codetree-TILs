5bb4b50771b7043713c513e1d7f56396ec37d15a
이도환 <71830104+lee0798@users.noreply.github.com>
2024-01-08 10:32:25 +0900

    Title: 최대로 겹치는 구간, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240108/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 240108/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -3,19 +3,21 @@
 using namespace std;
 
 int main() {
-    int arr[100] = {};
+    int arr[200] = {};
     int b;
     cin >> b;
 
     for (int i = 0; i < b; i++) {
         int st, end;
         cin >> st >> end;
+        st += 100;
+        rnd += 100;
         for (int j = st; j < end; j++) {
             arr[j]++;
         }
     }
 
-    int max_val = *max_element(arr, arr + 101);
+    int max_val = *max_element(arr, arr + 201);
     cout << max_val;
 
     return 0;
