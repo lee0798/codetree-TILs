16101144b4e1fac6879ad74e73590c3fbdff7297
이도환 <71830104+lee0798@users.noreply.github.com>
2024-01-08 13:23:18 +0900

    Title: 사각형의 총 넓이 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240108/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
+++ 240108/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -8,11 +8,11 @@ int main() {
     int arr[200][200] ={};
     for(int i = 0; i < n ; i ++){
         int x1,y1,x2,y2;
-        x1 += 100;
-        x2 += 100;
-        y1 += 100;
-        y2 += 100;
         cin >> x1 >> y1 >> x2 >> y2;
+        x1 += OFFSET;
+        x2 += OFFSET;
+        y1 += OFFSET;
+        y2 += OFFSET;
         for(int j = x1 ; j < x2; j ++){
             for(int k = y1; k < y2; k ++){
                 arr[j][k] += 1;
