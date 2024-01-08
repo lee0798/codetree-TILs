7697c1694b3762e363acfb4af407d0aa632923ad
이도환 <71830104+lee0798@users.noreply.github.com>
2024-01-08 10:34:25 +0900

    Title: 최대로 겹치는 지점, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240108/최대로 겹치는 지점/maximum-overlapped-points.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int arr[200] = {};
+    int b;
+    cin >> b;
+
+    for (int i = 0; i < b; i++) {
+        int st, end;
+        cin >> st >> end;
+        st += 100;
+        end += 100;
+        for (int j = st; j <= end; j++) {
+            arr[j]++;
+        }
+    }
+
+    int max_val = *max_element(arr, arr + 201);
+    cout << max_val;
+
+    return 0;
+}
