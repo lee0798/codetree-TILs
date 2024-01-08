9381b86609fdb918fd70ae77a77d5592da2d6166
이도환 <71830104+lee0798@users.noreply.github.com>
2024-01-08 09:45:56 +0900

    Title: 10진수로 변환하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240108/10진수로 변환하기/convert-to-decimal.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main() {
+    string binary;
+    cin >> binary;
+    int n = stoi(binary);
+    int num = 0;
+
+    for (int i = 0; i < binary.length(); i++) {
+        num += pow(2, i) * (n % 2);
+        n /= 2;
+    }
+
+    cout << num;
+
+    return 0;
+}
